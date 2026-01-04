ebfcc1b48887e4f1da23a61bfde360c29f17d777
Neo131088 <neo131088@gmail.com>
2026-01-04 12:46:02 +0100

    add

--- app/(private routes)/profile/edit/page.tsx
+++ app/(private routes)/profile/edit/page.tsx
@@ -1,84 +1,32 @@
-"use client";
 
-import Image from "next/image";
-import css from "./EditProfile.module.css";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { useAuthStore } from "@/lib/store/authStore";
-import { patchMe } from "@/lib/api/clientApi";
+'use client';
 
-function Edit() {
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuthStore } from '@/lib/store/authStore';
+export default function EditProfilePage() {
   const router = useRouter();
-  const { user, setUser } = useAuthStore();
-  const [userName, setUserName] = useState(user?.username);
+  const { user } = useAuthStore();
+  const [username, setUsername] = useState(user?.username ?? '');
+
+  useEffect(() => {
+    if (!user) {
+      router.push('/sign-in');
+    }
+  }, [user, router]);
 
   if (!user) {
-    router.push("/sign-in");
-    return null;
+    return null; // або <Loader />
   }
 
-  const handleSubmit = async (formData: FormData) => {
-    const newUserName = formData.get("username") as string;
-    const updateUser = await patchMe({ username: newUserName });
-    setUser(updateUser);
-    router.push("/profile");
-  };
-
-  const handleCancel = () => {
-    router.push("/profile");
-  };
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUserName(event.target.value);
-    console.log(event.target.value);
-  };
-
   return (
-    <main className={css.mainContent}>
-      <div className={css.profileCard}>
-        <h1 className={css.formTitle}>Edit Profile</h1>
+    <main>
+      <h1>Edit profile</h1>
 
-        {user.avatar && (
-          <Image
-            src={user.avatar}
-            alt="User Avatar"
-            width={120}
-            height={120}
-            className={css.avatar}
-          />
-        )}
-
-        <form className={css.profileInfo} action={handleSubmit}>
-          <div className={css.usernameWrapper}>
-            <label htmlFor="username">Username:</label>
-            <input
-              id="username"
-              name="username"
-              type="text"
-              value={userName}
-              className={css.input}
-              onChange={handleChange}
-            />
-          </div>
-
-          <p>Email: {user.email}</p>
-
-          <div className={css.actions}>
-            <button type="submit" className={css.saveButton}>
-              Save
-            </button>
-            <button
-              type="button"
-              className={css.cancelButton}
-              onClick={handleCancel}
-            >
-              Cancel
-            </button>
-          </div>
-        </form>
-      </div>
+      <input
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
     </main>
   );
 }
-
-export default Edit;
